fb4500e57cf92af3ce35cb7e61a1c3d624a24ac2
arkrasouski <arkrasouski@arortem.ru>
2026-07-01 22:12:32 +0300

    test: очень изменяю модели

--- dbt/hw1/macros/generate_schema_name.sql
+++ dbt/hw1/macros/generate_schema_name.sql
@@ -1,7 +1,7 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
   {%- set default_schema = target.schema -%}
   {%- if target.name == 'ci' -%}
-    {{ return('dbt_ci_' ~ env_var('DBT_CI_USER', 'unknown')) }}
+    {{ return('dbt_ci_' ~ env_var('GITHUB_USER', 'unknown')) }}
   {%- elif custom_schema_name is none -%}
     {{ return(default_schema) }}
   {%- else -%}
--- dbt/hw1/models/marts/core/mart__proc_accum_top10.sql
+++ dbt/hw1/models/marts/core/mart__proc_accum_top10.sql
@@ -12,6 +12,6 @@ SELECT
     processor_speed,
     battery_capacity
 FROM {{ ref('int__smartphones_combination') }}
-ORDER BY combination_count ASC
+ORDER BY combination_count DESC
 LIMIT 10
 
